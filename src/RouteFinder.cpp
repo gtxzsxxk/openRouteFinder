2462c8ba14a7a6ab8b812ea4c0bc0f29a4f6a04b
Hanyuan Zhao <hanyuan-z@qq.com>
2024-04-08 17:49:39 +0800

    RouteFinder: save status to ignore the large data copy before dijkstra

--- src/RouteFinder.cpp
+++ src/RouteFinder.cpp
@@ -6,23 +6,97 @@
 #include <queue>
 #include <vector>
 
+enum HISTORY_DATA_TYPE {
+    HISTORY_TYPE_DOUBLE,
+    HISTORY_TYPE_BOOL,
+    HISTORY_TYPE_POINTER,
+    HISTORY_TYPE_STRING,
+};
+
+struct WriteHistory {
+    void *Pointer;
+    HISTORY_DATA_TYPE DataType;
+    char Data[16];
+};
+
+template<class T>
+static void historyAppend(std::vector<WriteHistory> &History, T *Address) {
+    WriteHistory Record{};
+    Record.Pointer = Address;
+    if (std::is_same<T, double>::value) {
+        Record.DataType = HISTORY_TYPE_DOUBLE;
+    } else if (std::is_same<T, bool>::value) {
+        Record.DataType = HISTORY_TYPE_BOOL;
+    } else if (std::is_same<T, NavaidCompare *>::value) {
+        Record.DataType = HISTORY_TYPE_POINTER;
+    } else {
+        throw "No matched history type!";
+    }
+
+    auto StorePointer = reinterpret_cast<decltype(Address)>(Record.Data);
+    *StorePointer = *Address;
+    History.push_back(Record);
+}
+
+/* 其它都是传存变量地址的变量的地址，这里直接传字符串的地址 */
+static void historyAppend(std::vector<WriteHistory> &History, std::string *Address) {
+    WriteHistory Record{};
+    Record.Pointer = Address;
+    Record.DataType = HISTORY_TYPE_STRING;
+
+    auto StringData = *Address;
+
+    auto StorePointer = reinterpret_cast<char *>(Record.Data);
+    auto Length = StringData.size();
+    for (auto i = 0; i < Length; i++) {
+        *(StorePointer + i) = StringData[i];
+    }
+    *(StorePointer + Length) = 0;
+    History.push_back(Record);
+}
+
+static void historyRestore(WriteHistory &Record) {
+    if (Record.DataType == HISTORY_TYPE_DOUBLE) {
+        auto ReadPointer = reinterpret_cast<double *>(Record.Data);
+        auto RestorePointer = reinterpret_cast<double *>(Record.Pointer);
+        *RestorePointer = *ReadPointer;
+    } else if (Record.DataType == HISTORY_TYPE_BOOL) {
+        auto ReadPointer = reinterpret_cast<bool *>(Record.Data);
+        auto RestorePointer = reinterpret_cast<bool *>(Record.Pointer);
+        *RestorePointer = *ReadPointer;
+    } else if (Record.DataType == HISTORY_TYPE_POINTER) {
+        auto ReadPointer = reinterpret_cast<NavaidCompare **>(Record.Data);
+        auto RestorePointer = reinterpret_cast<NavaidCompare **>(Record.Pointer);
+        *RestorePointer = *ReadPointer;
+    } else if (Record.DataType == HISTORY_TYPE_STRING) {
+        auto ReadPointer = reinterpret_cast<char *>(Record.Data);
+        auto RestorePointer = reinterpret_cast<std::string *>(Record.Pointer);
+        *RestorePointer = std::string(ReadPointer);
+    } else {
+        throw "No matched history type!";
+    }
+}
+
 RouteResult
 RouteFinder::calculateShortestRoute(const NavaidInformation &Start, const NavaidInformation &End,
                                     AIRWAY_TYPE AirwayType) {
-    /* 由于状态与点的数据都绑定在一起，因此每次计算前都需要拷贝一份 */
-    auto NavCompares = NavCompareSourceData;
+
+    std::vector<WriteHistory> LocalHistory;
+
+    auto &NavCompares = NavCompareSourceData;
 
     auto realStartNode = NavaidCompare::getNavaidCompareFromMap(NavCompares, NavaidInformation::toUniqueKey(Start));
     auto realEndNode = NavaidCompare::getNavaidCompareFromMap(NavCompares, NavaidInformation::toUniqueKey(End));
 
-    if(!realStartNode) {
+    if (!realStartNode) {
         return {realEndNode};
     }
 
-    if(!realEndNode) {
+    if (!realEndNode) {
         return {};
     }
 
+    historyAppend(LocalHistory, &realStartNode->DistanceToStart);
     realStartNode->DistanceToStart = 0;
 
     std::priority_queue<NavaidCompare *, std::vector<NavaidCompare *>, NavaidCompare> q;
@@ -39,6 +113,7 @@ RouteFinder::calculateShortestRoute(const NavaidInformation &Start, const Navaid
         if (currentNode->ShortestDiscovered) {
             continue;
         }
+        historyAppend(LocalHistory, &currentNode->ShortestDiscovered);
         currentNode->ShortestDiscovered = true;
 
         for (const auto &Edge: currentNode->getEdges()) {
@@ -49,14 +124,22 @@ RouteFinder::calculateShortestRoute(const NavaidInformation &Start, const Navaid
             auto newDistance = *static_cast<NavaidInformation *>(currentNode) *
                                *static_cast<NavaidInformation *>(nextNode);
             if (currentNode->DistanceToStart + newDistance < nextNode->DistanceToStart) {
+                historyAppend(LocalHistory, &nextNode->DistanceToStart);
                 nextNode->DistanceToStart = currentNode->DistanceToStart + newDistance;
+                historyAppend(LocalHistory, &nextNode->ComeFrom);
                 nextNode->ComeFrom = currentNode;
+                historyAppend(LocalHistory, &nextNode->ViaEdge);
                 nextNode->ViaEdge = Edge.Name;
-                nextNode->Path = currentNode->Path + Edge.Name + " " + nextNode->getIdentifier() + "\n";
                 q.push(nextNode);
             }
         }
     }
 
-    return {realEndNode};
+    auto ResultObject = RouteResult(realEndNode);
+
+    for (long i = LocalHistory.size() - 1; i >= 0; i--) {
+        historyRestore(LocalHistory[i]);
+    }
+
+    return std::move(ResultObject);
 }
