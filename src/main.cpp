78ec133bc93a0621a601e88cec662c2d779b2d9c
Hanyuan Zhao <hanyuan-z@qq.com>
2024-04-10 09:49:20 +0800

    v2: search by ICAO demo

--- src/main.cpp
+++ src/main.cpp
@@ -16,35 +16,27 @@ int main() {
     t2 = std::chrono::steady_clock::now();
     dr_us = std::chrono::duration<double, std::micro>(t2 - t1).count();
 
-    reader.readAirportProcedure("ZGHA");
-
     t1 = std::chrono::steady_clock::now();
     reader.readAirways();
     t2 = std::chrono::steady_clock::now();
     dr_us = std::chrono::duration<double, std::micro>(t2 - t1).count();
 
     auto routeFinder = RouteFinder(reader);
-    while (1) {
-        std::string startIdentifier, startRegionCode;
-        std::string endIdentifier, endRegionCode;
+    while (true) {
+        std::string departure, arrival;
 
-        std::cout << "Start Node Identifier: ";
-        std::cin >> startIdentifier;
-        std::cout << "Start Node Region Code: ";
-        std::cin >> startRegionCode;
+        std::cout << "Departure ICAO: ";
+        std::cin >> departure;
 
-        std::cout << "End Node Identifier: ";
-        std::cin >> endIdentifier;
-        std::cout << "End Node Region Code: ";
-        std::cin >> endRegionCode;
+        std::cout << "Arrival ICAO: ";
+        std::cin >> arrival;
 
         t1 = std::chrono::steady_clock::now();
-        auto results = routeFinder.calculateShortestRoute(NavaidInformation(startIdentifier, startRegionCode),
-                                                          NavaidInformation(endIdentifier, endRegionCode));
+        auto results = routeFinder.calculateBetweenAirports(departure, arrival);
         t2 = std::chrono::steady_clock::now();
         std::cout << "Results: " << results;
         dr_us = std::chrono::duration<double, std::milli>(t2 - t1).count();
-        std::cout << "Time: " << dr_us << " (ms)" << std::endl;
+        std::cout << std::endl << "Time: " << dr_us << " (ms)" << std::endl;
     }
     return 0;
 }
